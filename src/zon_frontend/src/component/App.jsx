5b79f47552f5d54331ffc0e20ddddcda05255699
Victor Porton <porton.victor@gmail.com>
2023-05-23 18:31:44 +0300

    switched to React

---
+++ src/zon_frontend/src/component/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+// import { zon_backend } from "../../../declarations/zon_backend";
+
+export default function App() {
+    return (
+        <>
+            <h1>Zon Dir</h1>
+        </>
+    );
+}
