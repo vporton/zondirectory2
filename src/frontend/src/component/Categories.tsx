d774faea8759ff8729b750e33815af3a5561060f
Victor Porton <porton.victor@gmail.com>
2023-09-30 11:34:02 +0300

    simplified

--- src/frontend/src/component/Categories.tsx
+++ src/frontend/src/component/Categories.tsx
@@ -3,24 +3,22 @@ import { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 
 export default function Categories(props: { defaultCategories?: string[], onChange?: (categories: string[]) => void }) {
-    const [categories, setCategories] = useState<string[] | undefined>();
-    const [categoriesList, setCategoriesList] = useState((categories ?? []).map(c => c.toString()));
+    const [categories, setCategories] = useState<string[]>([]);
     useEffect(() => {
-        setCategoriesList(props.defaultCategories ?? []);
+        setCategories(props.defaultCategories ?? []);
     }, [props.defaultCategories])
     function updateCategories() {
-        setCategories(categoriesList);
-        if (props.onChange !== undefined && categoriesList !== undefined) {
-            props.onChange(categoriesList);
+        if (props.onChange !== undefined && categories !== undefined) {
+            props.onChange(categories);
         }
     }
-    useEffect(updateCategories, [categoriesList]);
+    useEffect(updateCategories, [categories]);
     function updateCategoriesList() {
         const list: string[] = [];
         for (const e of document.querySelectorAll('#categoriesList input') as any) {
             list.push((e as HTMLInputElement).value)
         }
-        setCategoriesList(list);
+        setCategories(list);
     }
 
     return (
@@ -28,16 +26,16 @@ export default function Categories(props: { defaultCategories?: string[], onChan
             <h2>Post to categories (TODO: Limited to ?? posts per day)</h2>
             <p>TODO: Visual editor of categories</p>
             <ul id="categoriesList">
-                {categoriesList.map((cat, i) => {
+                {categories.map((cat, i) => {
                     return (
                         <li key={i}>
                             <input value={cat} onChange={updateCategoriesList}/>
-                            <Button onClick={() => setCategoriesList(categoriesList.filter((item) => item !== cat))}>Delete</Button>
+                            <Button onClick={() => setCategories(categories.filter((item) => item !== cat))}>Delete</Button>
                         </li>
                     );
                 })}
             </ul>
-            <p><Button onClick={() => setCategoriesList(categoriesList.concat([""]))}>Add</Button></p>
+            <p><Button onClick={() => setCategories(categories.concat([""]))}>Add</Button></p>
         </>
     );
 }
