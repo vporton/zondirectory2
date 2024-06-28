bd27e5acc762b74a0e933ef272668d5377e71088
Victor Porton <porton.victor@gmail.com>
2024-06-28 22:11:13 +0300

    text

--- src/frontend/src/component/EditFoldersList.tsx
+++ src/frontend/src/component/EditFoldersList.tsx
@@ -97,7 +97,7 @@ export default function EditFoldersList(props: {
     return (
         <>
             <h1>{props.reverse ? `Folders to post` : `Post to folders`}</h1>
-            <p>TODO: Visual editor of folders; TODO: Limited to ?? posts per day; TODO: begin/end works only for owned folders</p>
+            <p>TODO: Visual editor of folders. TODO: begin/end works only for owned folders.</p>
             <Container>
                 <Row>
                     <Col>
