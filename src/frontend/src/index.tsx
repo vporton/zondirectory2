3839cb67d3638f99f7526d0b154f3e2c28086213
Victor Porton <porton.victor@gmail.com>
2025-01-05 22:46:33 +0200

    eliminate warning

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -3,5 +3,5 @@ import { createRoot } from 'react-dom/client';
 import App from "./component/App";
 
 const container = document.getElementById('app');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(<App/>);
